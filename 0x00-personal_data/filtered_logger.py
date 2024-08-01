01342cd474358d502e6d473488c91cdfea62c558
prisca2920 <prisca2920@gmail.com>
2024-08-01 14:17:49 +0000

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,3 +62,21 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         user=username,
         password=pswd)
     return conn
+
+
+def main() -> None:
+    """ takes no args and returns nothing"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        print(message)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
