490de936e8e0edb9b3c4688a816b14e266b5249a
prisca2920 <prisca2920@gmail.com>
2024-08-15 07:50:19 +0000

    destroy

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,7 +70,7 @@ class Auth:
         """destroys a session"""
         try:
             user = self._db.find_user_by(id=user_id)
-            self.db.update_user(user.id, session_id=None)
+            self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
         return None
