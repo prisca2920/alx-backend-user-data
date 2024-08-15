987b0d555b8d429620a1465c18891c0802fa9176
prisca2920 <prisca2920@gmail.com>
2024-08-15 08:57:25 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,3 +74,14 @@ class Auth:
         except NoResultFound:
             return None
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
