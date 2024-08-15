7d24d7197b954ee35697597b583039fed7007cc4
prisca2920 <prisca2920@gmail.com>
2024-08-15 12:09:58 +0000

    main

---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""tests for the auth codes
+"""
+import requests
+
+
+def register_user(email: str, password: str) -> None:
+    """
+    Test for register a user
+    """
+    resp = requests.post('http://127.0.0.1:5000/users',
+                         data={'email': email, 'password': password})
+    if resp.status_code == 200:
+        assert (resp.json() == {"email": email, "message": "user created"})
+    else:
+        assert(resp.status_code == 400)
+        assert (resp.json() == {"message": "email already registered"})
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """
+    Test for wrong password
+    """
+    r = requests.post('http://127.0.0.1:5000/sessions',
+                      data={'email': email, 'password': password})
+    assert (r.status_code == 401)
+
+
+def profile_unlogged() -> None:
+    """
+    Test for unlogged profile"""
+    r = requests.get('http://127.0.0.1:5000/profile')
+    assert(r.status_code == 403)
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
