365c46f6774b9a914b1c70b16c01f29179f92420
prisca2920 <prisca2920@gmail.com>
2024-08-15 10:19:14 +0000

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,7 +66,7 @@ def profile() -> str:
         abort(403)
 
 
-@app.route('/reset_password', methods=['POST'], strict_slashe=False)
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token() -> str:
     """getting a reset password"""
     email = request.form.get('email')
