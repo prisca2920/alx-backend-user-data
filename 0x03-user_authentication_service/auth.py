68b6bb01d4c746633a57ec16a2bb97fe43b1aba9
prisca2920 <prisca2920@gmail.com>
2024-08-15 07:28:47 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -54,3 +55,13 @@ class Auth:
         else:
             email.session_id = _generate_uuid()
             return email.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Find user by session ID"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
