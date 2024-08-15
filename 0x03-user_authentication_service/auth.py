dbe342771a29373dd59f92d557ecf7d17e948417
prisca2920 <prisca2920@gmail.com>
2024-08-15 07:47:39 +0000

    destroy

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,4 +72,5 @@ class Auth:
             user = self._db.find_user_by(id=user_id)
             self.db.update_user(user.id, session_id=None)
         except NoResultFound:
-            pass
+            return None
+        return None
