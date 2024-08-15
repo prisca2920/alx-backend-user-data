7f3f01110fff417ee3d9e5c328446da7ccaeca24
prisca2920 <prisca2920@gmail.com>
2024-08-15 06:55:17 +0000

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """creating a basic flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -25,6 +25,23 @@ def users() -> str:
         return jsonify({"email": f"{email}", "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """implement a login function"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = AUTH.valid_login(email, password)
+
+    if user:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": f"{email}", "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
